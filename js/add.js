c3f5795b4685df235c2a268cbb3628bd27d2376c
Christopher Yamas <yamas.chris@gmail.com>
2022-02-08 16:41:43 -0600

    changed console.logs to alerts, added comments

--- js/add.js
+++ js/add.js
@@ -1,10 +1,11 @@
 'use strict';
 
-// First Yes or No question prompt. less than thirty?, went to school on east coast?, worked for joe biden's campaign?, ever worked in Kansas?, has goal of completing the Code 401 for Java?
+// Initial name request prompt followed my alert greeting visitor with their name
 let user = prompt('Thanks for visiting this page! What is your name?');
 console.log('The user\'s name is ' + user)
-prompt('Hello ' + user + '! I hope you\'re ready for a guessing game! Let\'s see how well you know Christopher Yamas, before you get all the juicy details...');
+alert('Hello ' + user + '! I hope you\'re ready for a guessing game! Let\'s see how well you know Christopher Yamas, before you get all the juicy details...');
 
+// First About Me guessing question
 function ageGuessQuestion() {
   let ageGuess = prompt('Yes or No: is Christopher over thirty years old?').toLowerCase();
   if (ageGuess === 'y' || ageGuess === 'yes') {
@@ -21,53 +22,69 @@ function ageGuessQuestion() {
 }
 ageGuessQuestion()
 
+// Second About Me guessing question
 function coastSchoolQuestion() {
   let coastSchool = prompt('Yes or No: did Christopher earn his undergraduate degree from a university on the east coast of the U.S.?').toLowerCase();
   if (coastSchool === 'y' || coastSchool === 'yes') {
-    console.log('Nope! He graduated from a university in California!');
+    alert('Nope! He graduated from a university in California!');
+  //  console.log('Nope! He graduated from a university in California!');
   } else if (coastSchool === 'n' || coastSchool === 'no') {
-    console.log('Yup! He graduated from a university in California!');
+    alert('Yup! He graduated from a university in California!');
+  //  console.log('Yup! He graduated from a university in California!');
   } else {
-    console.log('You gotta respond in yes or no form!');
+    alert('You gotta respond in yes or no form!');
+  //  console.log('You gotta respond in yes or no form!');
     coastSchoolQuestion();
   }
 }
 coastSchoolQuestion()
 
+// Third About Me guessing question
 function bidenCampQuestion() {
   let bidenCamp = prompt('Yes or No: did Christopher work for the Joe Biden presidential campaign in 2020?').toLowerCase();
   if (bidenCamp === 'y' || bidenCamp === 'yes') {
-    console.log('Not that one! But he did work during the 2020 primary on Elizabeth Warren\'s campaign.');
+    alert('Not that one! But he did work during the 2020 primary on Elizabeth Warren\'s campaign.');
+  //  console.log('Not that one! But he did work during the 2020 primary on Elizabeth Warren\'s campaign.');
   } else if (bidenCamp === 'n' || bidenCamp === 'no') {
-    console.log('Correct! While he did not work for the Biden campaign, he did work during the primary for the Elizabeth Warren campaign.');
+    alert('Correct! While he did not work for the Biden campaign, he did work during the primary for the Elizabeth Warren campaign.');
+  //  console.log('Correct! While he did not work for the Biden campaign, he did work during the primary for the Elizabeth Warren campaign.');
   } else {
-    console.log('You gotta respond in yes or no form!');
+    alert('You gotta respond in yes or no form!');
+  //  console.log('You gotta respond in yes or no form!');
     bidenCampQuestion();
   }
 }
 bidenCampQuestion()
 
+// Fourth About Me guessing question
 function python401Question() {
   let python401 = prompt('SECOND TO LAST QUESTION! Yes or No: is one of Christopher\'s goals to take the Code 401 for PYTHON specifically?').toLowerCase();
   if (python401 === 'y' || python401 === 'yes') {
-    console.log('Yes! Not necessarily because he is partial to Python, but because he wants to complete his Code Fellows track as soon as possible.');
+    alert('Yes! Not necessarily because he is partial to Python, but because he wants to complete his Code Fellows track as soon as possible.');
+  //  console.log('Yes! Not necessarily because he is partial to Python, but because he wants to complete his Code Fellows track as soon as possible.');
   } else if (python401 === 'n' || python401 === 'no') {
-    console.log('Nope, he DOES want to take the Python course. He is looking to finish his Code Fellows track as soon as possible, and then he will dealve deeper into the other languages.');
+    alert('Nope, he DOES want to take the Python course. He is looking to finish his Code Fellows track as soon as possible, and then he will dealve deeper into the other languages.');
+  //  console.log('Nope, he DOES want to take the Python course. He is looking to finish his Code Fellows track as soon as possible, and then he will dealve deeper into the other languages.');
   } else {
-    console.log('You gotta respond in yes or no form!');
+    alert('You gotta respond in yes or no form!');
+  //  console.log('You gotta respond in yes or no form!');
     python401Question();
   }
 }
 python401Question()
 
+// Fifth About Me guessing question
 function remoteWorkQuestion() {
   let remoteWork = prompt('LAST QUESTION! Yes or No: would Christopher prefer to work remotely at his next job?').toLowerCase();
   if (remoteWork === 'y' || remoteWork === 'yes') {
-    console.log('Yes, he loves to have flexibility of location so he would definitely prefer to work remotely!');
+    alert('Yes, he loves to have flexibility of location so he would definitely prefer to work remotely!');
+  //  console.log('Yes, he loves to have flexibility of location so he would definitely prefer to work remotely!');
   } else if (remoteWork === 'n' || remoteWork === 'no') {
-    console.log('Nope, while he may still be game for an in-person job, his goal is to secure the best remote work job he can find!');
+    alert('Nope, while he may still be game for an in-person job, his goal is to secure the best remote work job he can find!');
+  //  console.log('Nope, while he may still be game for an in-person job, his goal is to secure the best remote work job he can find!');
   } else {
-    console.log('You gotta respond in yes or no form!');
+    alert('You gotta respond in yes or no form!');
+  //  console.log('You gotta respond in yes or no form!');
     remoteWorkQuestion();
   }
 }
